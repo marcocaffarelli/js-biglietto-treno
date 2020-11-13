23a40e5de9371e8f1439db4e500443bdcc9fbc8b
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-13 14:24:09 +0100

    Aggiunta seconda richiesta all'utente
    Aggiunta la richiesta di età all'utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 // chiedere all'utente il numero di km che intende percorrere
 var numeroKm = Number(prompt("quanti km intendi percorrere?"));
 console.log(numeroKm);
+// chiedere all'utente l'età
+var etaPasseggero = Number(prompt("scrivi la tua età?"));
+console.log(etaPasseggero);
